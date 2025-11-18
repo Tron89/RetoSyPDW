9c846a0a838158304e955100c01958cf139571ac
Tron89 <claudiuacircota@gmail.com>
2025-11-18 16:09:05 +0100

    Ups, lo tenia como ip en vez de localhost

--- back/RetoSyPDW/src/main/java/Main/main.java
+++ back/RetoSyPDW/src/main/java/Main/main.java
@@ -5,7 +5,7 @@ import jakarta.xml.ws.Endpoint;
 public class main {
 	public static void main(String[] args) {
 		Peticiones implementor = new Peticiones();
-		String address = "http://192.168.1.155:9000/Peticiones";
+		String address = "http://localhost:9000/Peticiones";
 		Endpoint.publish(address, implementor);
 	}
 }
